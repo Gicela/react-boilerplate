cb1a118b70be06555dfe09ad4ce6c575f19ee373
Gicela <gicela@mexicanwave.comq>
2017-03-25 11:42:35 +0000

    React boilerplate with js experimental features

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+console.log(objTwo);
 
 ReactDOM.render(
    <h1>Boilerplate app!</h1>,
--- public/bundle.js
+++ public/bundle.js
@@ -46,13 +46,26 @@
 
 	'use strict';
 
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 	var React = __webpack_require__(1);
 	var ReactDOM = __webpack_require__(158);
 
+	var objOne = {
+	    name: "Gicela",
+	    location: "Bristol"
+	};
+
+	var objTwo = _extends({
+	    age: 25
+	}, objOne);
+
+	console.log(objTwo);
+
 	ReactDOM.render(React.createElement(
-	  'h1',
-	  null,
-	  'Boilerplate app!'
+	    'h1',
+	    null,
+	    'Boilerplate app!'
 	), document.getElementById('app'));
 
 /***/ },
